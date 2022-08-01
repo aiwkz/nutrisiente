749fbf433ed416ddd8300818d1c52a79549dabb6
Fede <federico@justos.com>
2022-08-01 19:01:32 -0300

    🎉  initial project setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="App">
+    Nutrisiente
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.App {
+    display: flex;
+}
+</style>
